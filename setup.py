3f8e7b4c0cf5ee822d8bf805a8a352e4383e5f66
Franz A. Heinsen <franz@glassroom.com>
2022-11-22 09:17:24 -0500

    Change version number to conform to Python convention.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup
 
 setup(name='torch_train_test_loop',
-    version='1.0',
+    version='1.0.0',
     description='Composable training/testing of PyTorch deep learning models with minimal overhead.',
     url='https://github.com/glassroom/torch_train_test_loop',
     author='Franz A. Heinsen',
